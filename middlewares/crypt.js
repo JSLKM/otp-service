b2be1d0da958fa4fb28eb4edb64e3eb1fcaab28f
sjin <s.jin@be-tse.it>
2021-10-01 14:39:10 +0200

    changes

--- middlewares/crypt.js
+++ middlewares/crypt.js
@@ -1,13 +1,12 @@
 const crypto = require('crypto');
 
-// First we get our unique key to encrypt our object
-var password = process.env['CRYPT_PASSWORD'];
+const password = process.env['CRYPT_PASSWORD'];
 
 // We then get our unique Initialization Vector
-var iv =  Buffer.from(process.env['IV']);
+const iv =  Buffer.from(process.env['IV']);
 
 // To be used as salt in encryption and decryption
-var ivstring = iv.toString('hex');
+const ivstring = iv.toString('hex');
 
 // Function to find SHA1 Hash of password key
 function sha1(input) {
@@ -16,13 +15,13 @@ function sha1(input) {
 
 //Function to get secret key for encryption and decryption using the password
 function password_derive_bytes(password, salt, iterations, len) {
-    var key = Buffer.from(password + salt);
-    for (var i = 0; i < iterations; i++) {
+    const key = Buffer.from(password + salt);
+    for (const i = 0; i < iterations; i++) {
         key = sha1(key);
     }
     if (key.length < len) {
-        var hx = password_derive_bytes(password, salt, iterations - 1, 20);
-        for (var counter = 1; key.length < len; ++counter) {
+        const hx = password_derive_bytes(password, salt, iterations - 1, 20);
+        for (const counter = 1; key.length < len; ++counter) {
             key = Buffer.concat([key, sha1(Buffer.concat([Buffer.from(counter.toString()), hx]))]);
         }
     }
@@ -31,21 +30,21 @@ function password_derive_bytes(password, salt, iterations, len) {
 
 // Function to encode the object
 async function encode(string) {
-    var key = password_derive_bytes(password, '', 100, 32);
+    const key = password_derive_bytes(password, '', 100, 32);
     // Initialize Cipher Object to encrypt using AES-256 Algorithm 
-    var cipher = crypto.createCipheriv('aes-256-cbc', key, ivstring);
-    var part1 = cipher.update(string, 'utf8');
-    var part2 = cipher.final();
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, ivstring);
+    const part1 = cipher.update(string, 'utf8');
+    const part2 = cipher.final();
     const encrypted = Buffer.concat([part1, part2]).toString('base64');
     return encrypted;
 }
 
 // Function to decode the object
 async function decode(string) {
-    var key = password_derive_bytes(password, '', 100, 32);
+    const key = password_derive_bytes(password, '', 100, 32);
     // Initialize decipher Object to decrypt using AES-256 Algorithm
-    var decipher = crypto.createDecipheriv('aes-256-cbc', key, ivstring);
-    var decrypted = decipher.update(string, 'base64', 'utf8');
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, ivstring);
+    const decrypted = decipher.update(string, 'base64', 'utf8');
     decrypted += decipher.final();
     return decrypted;
 }
